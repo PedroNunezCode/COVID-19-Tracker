06241ff742eb024ee5455a1a743d00628ad9b6fb
PedroNunezCode <pedronunezcode@gmail.com>
2020-05-05 11:03:47 -0700

    Initial create-react-app boilerplate commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Initial Commit</h1>
+      <p>Cleaning up some of the boilerplate create-react-app made.</p>
+    </div>
+  )
+}
+
+export default App;

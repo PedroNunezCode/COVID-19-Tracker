3bb318ab53474014ef7f16c137105cb6496bbdc8
PedroNunezCode <pedronunezcode@gmail.com>
2020-05-05 14:27:33 -0700

    For now the basics of the page are done. Still need to update some things
    As of now everything seems to be pretty smooth. There is an error that I
    cannot seem to track but i'll get on that tomorrow. For now this will be it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+/**
+ * The following imports will be for the whole application, ranging
+ * from components to all the required css and libraries used to maintain
+ * the main application enpoint.
+ */
+import styles from './App.module.css';
+
+import { Cards, Chart, CountryPicker } from './Components';
+import { fetchData } from './api';
+
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState();
+
+  useEffect(() => {
+    async function getAPIData() {
+      setData(await fetchData());
+    }
+
+    getAPIData();
+  }, [setData]);
+
+  const handleCountryChange = async (country) => {
+     setData(await fetchData(country));
+     setCountry(country);
+  }
+
   return (
-    <div>
-      <h1>Hello Initial Commit</h1>
-      <p>Cleaning up some of the boilerplate create-react-app made.</p>
+    <div className={styles.container}>
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange}/>
+      <Chart data={data} country={country}/>
     </div>
   )
 }
